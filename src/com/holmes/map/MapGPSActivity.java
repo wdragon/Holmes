eb39d15379429e3d3a9114b5352cc93fefaea7ac
wdragon <wdragon521@gmail.com>
2011-05-08 16:07:26 -0700

    Added map activity

---
+++ src/com/holmes/map/MapGPSActivity.java
@@ -0,0 +1,28 @@
+package com.holmes.map;
+
+import com.holmes.R;
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class MapGPSActivity extends MapActivity {
+
+	private MapView mapView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
